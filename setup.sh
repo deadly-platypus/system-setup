ebc4c17c5333b3b35f92d65fea784c683cf37f37
Derrick McKee <derrick.mckee@gmail.com>
2021-10-27 08:41:27 -0400

    latest

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-execute_function() {
+execute_cmd() {
 	eval "$*"
 	retval=$?
 	if [ $retval -ne 0 ]; then
@@ -9,5 +9,10 @@ execute_function() {
 	fi
 }
 
-execute_function pamac update -a
-execute_function 
+os=$(cat /etc/os-release | awk -F = '{ if($1 == "ID") { print $2; } }')
+
+execute_cmd pamac update -a
+execute_cmd pamac install $(cat packages.$os.txt)
+execute_cmd chsh /bin/zsh
+execute_cmd chezmoi init git@github.com:deadly-platypus/dotfiles.git
+execute_cmd chezmoi apply
